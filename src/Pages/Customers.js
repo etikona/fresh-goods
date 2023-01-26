c43bf4b16511f96dd6aea1f0e811fc82ec23cae4
eti kona <etikonapal@gmail.com>
2023-01-27 04:09:56 +0600

    delete customer from ui and databaase

--- src/Pages/Customers.js
+++ src/Pages/Customers.js
@@ -1,39 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { toast } from 'react-hot-toast';
 import { useLoaderData } from 'react-router-dom';
 
 const Customers = () => {
     const customers = useLoaderData();
-    console.log(customers);
+    const [customerUi, setCustomerUi] = useState(customers)
+    // Remove Customer 
+    const customerDelete = _id => {
+        const agree = window.confirm("Are you sure to remove?");
+        if(agree){
+            fetch(`http://localhost:5000/customers/${_id}`, {
+                method: 'DELETE',
+            })
+            .then(res => res.json())
+            .then(data => {
+                if(data.deletedCount > 0){
+                    toast.success('One customer deleted successfully.');
+                    const remainingCustomers = customerUi.filter(customer => customer._id !== _id);
+                    setCustomerUi(remainingCustomers);
+                }
+            })
+        }
+    }
     return (
         <div>
             <h2>All customers are here</h2>
-            <p>Customers: {customers.length}</p>
+            <p>Customers: {customerUi.length}</p>
+            {/* Show all customers data in a table */}
             <div>
-            <div className="overflow-x-auto">
-  <table className="table w-full">
- 
-    <thead>
-      <tr>
-        <th></th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Edit</th>
-        <th>Delete</th>
-      </tr>
-    </thead>
-    <tbody>
-  {
-    customers.map((customer, i) => <tr key={customer._id}>
-        <td>{i+1}</td>
-        <td>{customer.displayName}</td>
-        <td>{customer.email}</td>
-        <td><button className='btn btn-sm btn-primary'>Edit</button></td>
-        <td><button className='btn btn-sm btn-error'>Delete</button></td>
-    </tr>)
-  }
-    </tbody>
-  </table>
-</div>
+                <div className="overflow-x-auto">
+                    <table className="table w-full">
+
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th>Name</th>
+                                <th>Email</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                customerUi.map((customer, i) => <tr key={customer._id}>
+                                    <td>{i + 1}</td>
+                                    <td>{customer.displayName}</td>
+                                    <td>{customer.email}</td>
+                                    <td><button className='btn btn-sm btn-primary'>Edit</button></td>
+                                    <td><button onClick={()=> customerDelete(customer._id)} className='btn btn-sm btn-error'>Delete</button></td>
+                                </tr>)
+                            }
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
